63c94de5714e856f487a7d73b1b3b3d4827df9e2
Joel Barranco <joealebarranco@gmail.com>
2022-10-07 19:11:54 -0300

    Todo List

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,26 +1,44 @@
-import React from "react";
-
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
+import React, { useState } from "react";
 
 //create your first component
+
 const Home = () => {
-	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
-		</div>
-	);
-};
+	const [tarea, setTarea] = useState([]);
+	const [tareaInput, setTareaInput] = useState("");
+
+	const addTask = (e) => {
+		e.preventDefault();
+		if (tareaInput !== ""){
+			setTarea([...tarea, tareaInput]);
+			setTareaInput("");
+		}
+	}
+	const deleteTask = (index) => {
+		const newTask = [...tarea];
+		newTask.splice(index, 1);
+		setTarea(newTask);
+		
+	}	
+	
+	return(
+	<div className = "pared">
+		<h1>todos</h1>
+		<form className = "tareas" onClick={addTask}>
+		<div className = "input">
+			<input type="text" autoComplete="off" value={tareaInput} placeholder = "Que tengo que hacer?" onChange={e => setTareaInput(e.target.value)}/>
+		</div>	
+		<button type="submit" className="addButton"></button>		
+		{tarea.map((item,index) => (			
+			<div className="task" key={index}>
+				<div className="newItem" key={index}>{item}</div>
+				<button className="delete" onClick={deleteTask}>x</button>
+           </div>
+		))}
+			<p>Pendientes: {tarea.length}</p>
+		</form>
+	</div>);
+	
+}
+
 
 export default Home;
